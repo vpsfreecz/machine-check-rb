418fe40734082b945642f915efd7f7f37628d1d2
Jakub Skokan <jakub.skokan@havefun.cz>
2022-10-04 11:18:07 +0200

    collectors/zfs: reverse success/fail values

--- lib/machine-check/collectors/zfs.rb
+++ lib/machine-check/collectors/zfs.rb
@@ -32,17 +32,17 @@ module MachineCheck
       list = `zpool list -Hp -o name,health,fragmentation,capacity`
 
       if $?.exitstatus != 0
-        @zpool_list_success.set(1)
+        @zpool_list_success.set(0)
         return
       end
 
-      @zpool_list_success.set(0)
-      @zpool_list_parse_success.set(0)
+      @zpool_list_success.set(1)
+      @zpool_list_parse_success.set(1)
       
       list.split("\n").each do |line|
         name, health, fragmentation, capacity = line.split
 
-        @zpool_list_health.set(health == 'ONLINE' ? 0 : 1, labels: {pool: name})
+        @zpool_list_health.set(health == 'ONLINE' ? 1 : 0, labels: {pool: name})
         @zpool_list_fragmentation.set(fragmentation.to_i, labels: {pool: name})
         @zpool_list_capacity.set(capacity.to_i, labels: {pool: name})
       end
